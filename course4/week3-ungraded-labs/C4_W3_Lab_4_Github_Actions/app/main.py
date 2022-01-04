11f3ded6aaa64646ad107631e035e5e8ce27992d
Sanjoy Datta <SanjayDatta@users.noreply.github.com>
2022-01-04 13:25:55 +0530

    trying the changes again

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-# Something better
+# Something even better
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
